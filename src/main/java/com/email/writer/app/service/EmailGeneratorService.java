b8ae71769120c451f16a09bafe348e74b219a301
Chaitanya_Anchal <chaitanyaanchal3107@gmail.com>
2025-10-28 01:35:27 +0530

    Used Spring Reactive Web(Web Flux) to make api request to handle asynchronous non-blocking http request and response

--- src/main/java/com/email/writer/app/service/EmailGeneratorService.java
+++ src/main/java/com/email/writer/app/service/EmailGeneratorService.java
@@ -49,13 +49,13 @@ public class EmailGeneratorService {
 //      here for making an api call we make use of web client, way to do a api request, build on top of project reactor, and enable us to handle asynchronous non-blocking http request and responses
 //        it makes this thing well suited for modern reactive web application
 
-//        String response = webClient.post() // sending data (the email prompt) using POST
-//                .uri(geminiApiUrl+geminiApiKey)  // This sets the API endpoint URL you’re calling.
-//                .header("Content-Type","application/json") // This means: I’m sending data in JSON format.
-//                .retrieve() //  It actually sends the request to the Gemini API, Gemini responds, .retrieve() prepares to receive that response
-//                .bodyToMono(String.class) //  ake the response body and convert it into a String wrapped in a Mono, we have two Mono for single reply and Flux for stream of reply
-//                // Mono<String> = a “promise” that one String value will arrive in the future, It’s part of the reactive programming idea — the response isn’t here yet, but it will come.
-//                .block(); // It blocks the current thread until the Gemini API sends the reply, synchronous
+        String response = webClient.post() // sending data (the email prompt) using POST
+                .uri(geminiApiUrl+geminiApiKey)  // This sets the API endpoint URL you’re calling.
+                .header("Content-Type","application/json") // This means: I’m sending data in JSON format.
+                .retrieve() //  It actually sends the request to the Gemini API, Gemini responds, .retrieve() prepares to receive that response
+                .bodyToMono(String.class) //  ake the response body and convert it into a String wrapped in a Mono, we have two Mono for single reply and Flux for stream of reply
+                // Mono<String> = a “promise” that one String value will arrive in the future, It’s part of the reactive programming idea — the response isn’t here yet, but it will come.
+                .block(); // It blocks the current thread until the Gemini API sends the reply, synchronous
 
         // Step 4
         // Extract response from gemini api format and Return it
